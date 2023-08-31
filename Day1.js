e126f7b4315aeaabd55ebb25af15dd82dd1d6cd7
rajan3210amour <123243673+rajan3210amour@users.noreply.github.com>
2023-08-31 18:20:02 +0000

    DataType of JavaScript

--- Day1.js
+++ Day1.js
@@ -1,1 +1,22 @@
 console.log("Gnanamani College Technology")
+console.log("This is Enviroment Science");
+
+// Array Function 
+
+let a = [1,2,3,4,5];
+let b = [3,5,6,7,4]
+
+let c = a.concat(b)
+console.log(c);
+
+// We want to build the Ecommerace 
+
+// syllbus of The JavaScript 
+
+// DataTyepe - Variable and Constant 
+
+
+
+
+
+
